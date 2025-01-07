06e63214816afcea672f2c7008765fad9848cac8
Minkey99 <songkc5671@naver.com>
2025-01-07 16:52:21 +0900

    Room 로직 개선
    * RoomEnterResponse DTO에 참가자들 정보들을 Set<> 자료구조로 중복없이 저장
    * Parameter가 길어짐에 따라 세로로 정렬함
    * 나머지 RoomMapper와 RoomService도 Set<> 자료구조를 이용해서 인게임 정보를 주고받는 로직을 추가
    * RoomController 는 log 정보 추가 ( 삭제 예정 )

--- src/main/java/com/example/quiz/dto/room/response/RoomEnterResponse.java
+++ src/main/java/com/example/quiz/dto/room/response/RoomEnterResponse.java
@@ -1,4 +1,16 @@
 package com.example.quiz.dto.room.response;
 
-public record RoomEnterResponse (Long roomId, String roomName, Long topicId, Integer maxPeople, Integer quizCount, boolean removeStatus){
-}
+import com.example.quiz.vo.InGameUser;
+import lombok.ToString;
+import java.util.Set;
+
+public record RoomEnterResponse (
+        Long roomId,
+        String roomName,
+        Long topicId,
+        Integer maxPeople,
+        Integer quizCount,
+        boolean removeStatus,
+        boolean isAdmin,
+        Set<InGameUser> participants
+) {}
