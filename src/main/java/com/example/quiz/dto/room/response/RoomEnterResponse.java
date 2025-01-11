f31d33da88162ce554c133937f9d173a85213a53
BHC-Chicken <tij05078@gmail.com>
2025-01-12 03:19:33 +0900

    방 입장시 반환되는 DTO 수정
     * RoomEnterResponse.java : 현재 참가중인 InGameUser 정보 전달.
    * room.html : 전체 표시가 아닌 참가자 한명의 정보만 노출되게 수정

--- src/main/java/com/example/quiz/dto/room/response/RoomEnterResponse.java
+++ src/main/java/com/example/quiz/dto/room/response/RoomEnterResponse.java
@@ -1,7 +1,8 @@
 package com.example.quiz.dto.room.response;
 
+import com.example.quiz.enums.Role;
 import com.example.quiz.vo.InGameUser;
-import lombok.ToString;
+
 import java.util.Set;
 
 public record RoomEnterResponse (
@@ -11,7 +12,7 @@ public record RoomEnterResponse (
         Integer maxPeople,
         Integer quizCount,
         boolean removeStatus,
-        boolean isAdmin,
-        boolean isInGame,
+        Role role,
+        InGameUser inGameUser,
         Set<InGameUser> participants
 ) {}
